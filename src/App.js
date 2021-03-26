b4c0ab58e4f4d6a3d53891bfa0cc6185e55172a4
karvaton <karvaton220@gmail.com>
2021-03-26 16:53:19 +0200

    розділення компонентів

--- src/App.js
+++ src/App.js
@@ -21,48 +21,92 @@ const list = [
 	},
 ];
 
+const isSearched = searchTerm => item => 
+    item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+class Search extends Component {
+    render() {
+        const {value, onChange} = this.props;
+        return (
+            <form>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                />
+            </form>
+        )
+    }
+}
+
+class Table extends Component {
+    render() {
+        const { list, pattern, onDismiss } = this.props;
+        return (
+            <div>
+            {list.filter(isSearched(pattern)).map(item =>
+                <div key={item.objectID}>
+                    <p>
+                        <a href={item.url}>{item.title}</a>
+                    </p>
+                    <p>{item.author}</p>
+                    <p>{item.num_comments}</p>
+                    <p>{item.points}</p>
+                    <p>
+                        <button
+                            onClick={() => onDismiss(item.objectID)}
+                            type="button"
+                        >
+                            Відкинути
+                        </button>
+                    </p>
+                </div>
+            )}
+            </div>
+        )
+    }
+}
+
 
 class App extends Component {
 	constructor(props) {
-    super(props);
+		super(props);
 
-    this.state = {
-      list,
+		this.state = {
+			list,
+            searchTerm: '',
+		}
+
+        this.onSearchChange = this.onSearchChange.bind(this);
+        this.onDismiss = this.onDismiss.bind(this);
+	}
+
+    onSearchChange(event) {
+        this.setState({ searchTerm: event.target.value });
     }
 
-    this.onDismiss.bind(this);
-  }
+	onDismiss(id) {
+		const isNotId = item => item.objectID !== id;
+		const updatedList = this.state.list.filter(isNotId);
+		this.setState({ list: updatedList });
+	}
 
-  onDismiss(id) {
-    const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        {this.state.list.map(item =>
-          <div key={item.objectID}>
-            <p>
-              <a href={item.url}>{item.title}</a>
-            </p>
-            <p>{item.author}</p>
-            <p>{item.num_comments}</p>
-            <p>{item.points}</p>
-            <p>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button"
-              >
-                Відкинути
-              </button>
-            </p>
-          </div>
-        )}
-      </div>
-    );
-  }
+	render() {
+        const {searchTerm, list} = this.state;
+		return (
+			<div className="App">
+                <Search
+                    value={searchTerm}
+                    onChange={this.onSearchChange}
+                />
+                <Table 
+                    list={list}
+                    pattern={searchTerm}
+                    onDismiss={this.onDismiss}
+                />				
+			</div>
+		);
+	}
 }
 
 export default App;
