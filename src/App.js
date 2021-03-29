3b9ff3b85dd47da8a09c5acea04987e2538a9489
karvaton <karvaton220@gmail.com>
2021-03-29 21:35:11 +0300

    розділення компонентів і перетворення

--- src/App.js
+++ src/App.js
@@ -24,48 +24,48 @@ const list = [
 const isSearched = searchTerm => item => 
     item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
-class Search extends Component {
-    render() {
-        const {value, onChange} = this.props;
-        return (
-            <form>
-                <input
-                    type="text"
-                    value={value}
-                    onChange={onChange}
-                />
-            </form>
-        )
-    }
-}
+const Search = ({value, onChange, children}) => 
+    <form>
+        {children}
+        <input
+            type="text"
+            value={value}
+            onChange={onChange}
+        />
+    </form>
 
-class Table extends Component {
-    render() {
-        const { list, pattern, onDismiss } = this.props;
-        return (
-            <div>
-            {list.filter(isSearched(pattern)).map(item =>
-                <div key={item.objectID}>
-                    <p>
-                        <a href={item.url}>{item.title}</a>
-                    </p>
-                    <p>{item.author}</p>
-                    <p>{item.num_comments}</p>
-                    <p>{item.points}</p>
-                    <p>
-                        <button
-                            onClick={() => onDismiss(item.objectID)}
-                            type="button"
-                        >
-                            Відкинути
-                        </button>
-                    </p>
-                </div>
-            )}
+
+const Table = ({ list, pattern, onDismiss }) =>
+    <div>
+        {list.filter(isSearched(pattern)).map(item =>
+            <div key={item.objectID}>
+                <span>
+                    <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+                <span>
+                    <Button
+                        onClick={() => onDismiss(item.objectID)}
+                    >
+                        Відкинути
+                    </Button>
+                </span>
             </div>
-        )
-    }
-}
+        )}
+    </div>
+
+
+const Button = ({ onClick, className = '', children }) =>
+    <button
+        onClick={onClick}
+        className={className}
+        type="button"
+    >
+        {children}
+    </button>
+
 
 
 class App extends Component {
@@ -98,7 +98,9 @@ class App extends Component {
                 <Search
                     value={searchTerm}
                     onChange={this.onSearchChange}
-                />
+                >
+                    Пошук
+                </Search>
                 <Table 
                     list={list}
                     pattern={searchTerm}
